eab21b6d357573095eda82060d00da9c94935031
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2022-10-23 17:35:17 -0700

    add text functions

--- pipeline/text_to_text_pipeline.py
+++ pipeline/text_to_text_pipeline.py
@@ -1,7 +1,12 @@
+from lib2to3.pgen2 import token
 import torch
 import json 
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 
+def preprocess_text(t):
+    """ Room for additional preprocessing"""
+
+    return t.strip().replace("\n","")
 
 def run_text_to_text_example():
     model = T5ForConditionalGeneration.from_pretrained('t5-small')
@@ -14,23 +19,26 @@ def run_text_to_text_example():
      At the daily White House coronavirus briefing on Wednesday, Trump said new guidelines to reopen the country would be announced on Thursday after he speaks to governors.
      "We'll be the comeback kids, all of us," he said. "We want to get our country back."  The Trump administration has previously fixed May 1 as a possible date to reopen the world's largest economy, but the president said some states may be able to return to normalcy earlier than that.
     """
+    summary = summarize(model, tokenizer, device, text)
+    print ("\n\nSummarized text: \n",summary)
 
+def summarize(model, tokenizer, device, text):
 
-    preprocess_text = text.strip().replace("\n","")
-    t5_prepared_Text = "summarize: "+preprocess_text
-    print ("original text preprocessed: \n", preprocess_text)
+    preprocessed_text = preprocess_text(text)
+    t5_prepared_Text = "summarize: "+preprocessed_text
+    print ("original text preprocessed: \n", preprocessed_text)
 
     tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
 
 
     # summmarize 
     summary_ids = model.generate(tokenized_text,
-                                        num_beams=4,
-                                        no_repeat_ngram_size=2,
-                                        min_length=30,
-                                        max_length=100,
-                                        early_stopping=True)
+                                    num_beams=4,
+                                    no_repeat_ngram_size=2,
+                                    min_length=30,
+                                    max_length=75,
+                                    early_stopping=True)
 
     output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-    print ("\n\nSummarized text: \n",output)
+    return output
